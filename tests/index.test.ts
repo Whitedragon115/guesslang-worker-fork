169664e9a64a0ff3dbcc590842467369259d6695
lawvs <18554747+lawvs@users.noreply.github.com>
2024-03-02 02:21:13 +0800

    fix: update tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,52 +1,5 @@
+import { describe, expect, it } from "vitest";
 import app from "../src";
-import { unstable_dev } from "wrangler";
-import type { UnstableDevWorker } from "wrangler";
-import { describe, expect, it, beforeAll, afterAll } from "vitest";
-
-// describe("Worker", () => {
-//   let worker: UnstableDevWorker;
-
-//   beforeAll(async () => {
-//     worker = await unstable_dev("src/index.ts", {
-//       experimental: { disableExperimentalWarning: true },
-//     });
-//   });
-
-//   afterAll(async () => {
-//     await worker.stop();
-//   });
-
-//   it("should return 200 response", async () => {
-//     const resp = await worker.fetch("/");
-//     expect(resp.status).toBe(200);
-//   });
-
-//   it("should get guesslang works", async () => {
-//     const resp = await worker.fetch("/guess?text=console.log");
-//     expect(resp.status).toBe(200);
-//     expect(await resp.json()).toMatchObject({
-//       confidence: 0.08425559401512146,
-//       reliable: false,
-//       languageId: "css",
-//       languageName: "CSS",
-//     });
-//   });
-
-//   it("should post guesslang works", async () => {
-//     const resp = await worker.fetch("/guess", {
-//       method: "POST",
-//       body: JSON.stringify({ text: 'import { Hono } from "hono";' }),
-//     });
-//     expect(resp.status).toBe(200);
-//     expect(resp.headers.get("Access-Control-Allow-Origin")).toBe("*");
-//     expect(await resp.json()).toMatchObject({
-//       confidence: 0.09199294000864029,
-//       reliable: false,
-//       languageId: "ts",
-//       languageName: "TypeScript",
-//     });
-//   });
-// });
 
 describe("Test the application", () => {
   it("Should return 200 response", async () => {
@@ -60,8 +13,29 @@ describe("Test the application", () => {
     expect(await res.text()).toBe("Missing query parameter 'text'");
   });
 
-  it("Should guesslang", async () => {
-    const res = await app.request("http://localhost/guess?text=import");
-    expect(res.status).toBe(200);
+  it("should get guesslang works", async () => {
+    const resp = await app.request("/guess?text=console.log");
+    expect(resp.status).toBe(200);
+    expect(await resp.json()).toMatchObject({
+      confidence: 0.08425559401512146,
+      reliable: false,
+      languageId: "css",
+      languageName: "CSS",
+    });
+  });
+
+  it("should post guesslang works", async () => {
+    const resp = await app.request("/guess", {
+      method: "POST",
+      body: JSON.stringify({ text: 'import { Hono } from "hono";' }),
+    });
+    expect(resp.status).toBe(200);
+    expect(resp.headers.get("Access-Control-Allow-Origin")).toBe("*");
+    expect(await resp.json()).toMatchObject({
+      confidence: 0.09199294000864029,
+      reliable: false,
+      languageId: "ts",
+      languageName: "TypeScript",
+    });
   });
 });
