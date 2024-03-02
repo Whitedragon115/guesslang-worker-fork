ccf8e0efad7b90a4a3fbe53fe99928cb868aebe2
lawvs <18554747+lawvs@users.noreply.github.com>
2024-03-03 00:22:50 +0800

    feat: add footer and ribbon components

---
+++ src/client/footer.tsx
@@ -0,0 +1,41 @@
+import { css } from "hono/css";
+
+export const Footer = () => {
+  return (
+    <footer
+      class={css`
+        padding-top: 32px;
+        /* border-top: 1px solid #e2e2e3; */
+        margin: 16px 0;
+        text-align: center;
+        line-height: 24px;
+        font-size: 14px;
+        font-weight: 500;
+        color: rgb(60 60 67 / 78%);
+      `}
+    >
+      <div class="container">
+        <p class="message">
+          Powered by{" "}
+          <a
+            href="https://github.com/yoeo/guesslang"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Guesslang
+          </a>{" "}
+          | Released under the{" "}
+          <a
+            href="https://github.com/lawvs/guesslang-worker/?tab=MIT-1-ov-file"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            MIT License
+          </a>
+          .
+        </p>
+        <p class="copyright">Copyright © 2024 whitewater</p>
+      </div>
+    </footer>
+  );
+};
