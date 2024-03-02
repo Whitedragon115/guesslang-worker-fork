30754308c1d565942efdadf6eeded60b83084542
lawvs <18554747+lawvs@users.noreply.github.com>
2024-03-02 12:41:16 +0800

    Revert "refactor: simplify vscode-languagedetection module"
    This reverts commit c94fac195627b73cc4dcf96bc3d9b064bedb4b5c.

--- tests/vscode-languagedetection.test.ts
+++ tests/vscode-languagedetection.test.ts
@@ -5,13 +5,11 @@ import child_process from "child_process";
 describe("vscode-languagedetection", () => {
   it("should match baseline", async () => {
     const exec = util.promisify(child_process.exec);
-    try {
-      const result = await exec(
-        "diff node_modules/@vscode/vscode-languagedetection/lib/index.ts src/vscode-languagedetection/index.ts",
-      );
-      expect(result.stdout).toBe("");
-    } catch (e) {
-      expect.unreachable();
-    }
+    const result = await exec(
+      "diff --rcs node_modules/@vscode/vscode-languagedetection/lib/index.ts src/vscode-languagedetection/index.ts || true",
+    );
+    expect(result.stdout).toMatchFileSnapshot(
+      "__snapshots__/vscode-languagedetection.rcs",
+    );
   });
 });
