a74cd712e50f87a64fab420737742aec2bd3d648
lawvs <18554747+lawvs@users.noreply.github.com>
2024-03-03 02:17:36 +0800

    test: add tests for expectedRelativeConfidence

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from "vitest";
 import app from "../src";
+import { DetectionResult } from "../src/types";
 
 describe("Test the application", () => {
   it("Should return 200 response", async () => {
@@ -37,5 +38,29 @@ describe("Test the application", () => {
       languageId: "ts",
       languageName: "TypeScript",
     });
+  });
+
+  it("should get expectedRelativeConfidence guesslang works", async () => {
+    const resp = await app.request(
+      "/guess?text=import&expectedRelativeConfidence=0",
+    );
+    expect(resp.status).toBe(200);
+    const json = await resp.json<DetectionResult>();
+    expect(json.confidence).toMatchObject(0.0883780375123024);
+    expect(json.reliable).toMatchObject(true);
+  });
+
+  it("should post expectedRelativeConfidence guesslang works", async () => {
+    const resp = await app.request("/guess", {
+      method: "POST",
+      body: JSON.stringify({
+        text: "import",
+        expectedRelativeConfidence: 0,
+      }),
+    });
+    expect(resp.status).toBe(200);
+    const json = await resp.json<DetectionResult>();
+    expect(json.confidence).toMatchObject(0.0883780375123024);
+    expect(json.reliable).toMatchObject(true);
   });
 });
